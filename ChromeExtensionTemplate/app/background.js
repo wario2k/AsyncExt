dd7c7603116745009e7379c172559d772005e703
bpm8lck <abadeshrestha@ups.com>
2019-06-27 15:55:33 -0400

    added notifcation creation

--- ChromeExtensionTemplate/app/background.js
+++ ChromeExtensionTemplate/app/background.js
@@ -1,5 +1,34 @@
 ﻿'use strict';
 
-chrome.runtime.onInstalled.addListener(function () {
-    console.log("Extension is loaded.");
-});
+function show() {
+    var time = /(..)(:..)/.exec(new Date());
+    var hour = time[1] % 12 || 12;
+    var period = time[1] < 12 ? 'a.m' : 'p.m';
+    new Notification(hour + time[2] + ' ' + period, {
+        body: 'Testing a Toast.'
+    })
+}
+
+if (!localStorage.isInitialized) {
+    localStorage.isActivated = true;
+    localStorage.frequency = 1;
+    localStorage.isInitialized = true;
+}
+
+if (window.Notification) {
+    if (JSON.parse(localStorage.isActivated)){ show(); }
+
+    var interval = 0; //display interval in minutes 
+
+    setInterval(function () {
+        interval++;
+        if (
+            JSON.parse(localStorage.isActivated &&
+                localStorage.frequency <= interval)
+        ) {
+            show();
+            interval = 0;
+        }
+    }
+        , 60000);//timeout
+}
